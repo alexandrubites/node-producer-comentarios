c98711c641f681f61c27d286c22ff5ec6a9f6da0
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-19 19:44:23 -0600

    cambios

---
+++ index.js
@@ -0,0 +1,48 @@
+const cors = require('cors')
+const express = require('express')
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: [' my-kafka-0.my-kafka-headless.alexandrubites.svc.cluster.local:9092']
+});
+
+const producer = kafka.producer()
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+
+const port = 8080;
+
+app.get('/', (req, res, next) => {
+  res.send('comments api - vladimir123');
+});
+
+const run = async (uId, oId, comment) => {
+
+    await producer.connect()
+//    await producer.send()
+    await producer.send({
+      topic: 'comments',
+      messages: [ 
+    { 
+      'value': `{ "userId": "${uId}",  "objectId": "${oId}", "comment": "${comment}"}`
+    } 
+      ],
+    })
+   await producer.disconnect()
+}
+
+app.get('/comments', (req, res, next) => {
+  const uId = req.query.userId;
+  const oId = req.query.objectId;
+  const comment = req.query.comment;
+  res.send({'userId:': uId, 'objectID': oId,'comment' : comment} );
+  run(uId, oId, comment).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+app.listen(port,  () => 
+	console.log('listening on port ' + port
+));
